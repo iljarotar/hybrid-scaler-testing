44c0f92e746c713349212a377ba6ae0d00416b2d
Ilja Rotar <kontakt@ilja-rotar.de>
2023-12-13 17:49:46 +0100

    make locust manifest more flexible

--- locust/run.sh
+++ locust/run.sh
@@ -3,9 +3,9 @@
 host=$1
 
 if [ -z "$host" ]; then
-  host="http://scalesserver.app.svc.cluster.local:8080"
+  host="http://localhost:8080"
 fi
 
 echo "starting locust at $host"
 
-locust --headless --users 10 --spawn-rate 1 -H $host -t 60s
+locust --headless --users 10 --spawn-rate 1 -H $host -t 10s
