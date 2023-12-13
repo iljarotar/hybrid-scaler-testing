ed02daebd3487aaac57cebbc71ea25eafecd90ca
Ilja Rotar <kontakt@ilja-rotar.de>
2023-12-13 18:12:26 +0100

    Revert "make locust manifest more flexible"
    This reverts commit 44c0f92e746c713349212a377ba6ae0d00416b2d.

--- locust/run.sh
+++ locust/run.sh
@@ -3,9 +3,9 @@
 host=$1
 
 if [ -z "$host" ]; then
-  host="http://localhost:8080"
+  host="http://scalesserver.app.svc.cluster.local:8080"
 fi
 
 echo "starting locust at $host"
 
-locust --headless --users 10 --spawn-rate 1 -H $host -t 10s
+locust --headless --users 10 --spawn-rate 1 -H $host -t 60s
