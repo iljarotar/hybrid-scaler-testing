716c3be3e23bda72f2d33077af1ca7f0ae2e8ea7
Ilja Rotar <kontakt@ilja-rotar.de>
2024-01-15 18:07:17 +0100

    add headless arg to run.sh and locust deployment

--- locust/run.sh
+++ locust/run.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 
 host=$1
+headless=$2
 
 if [ -z "$host" ]; then
   host="http://localhost:8080"
@@ -8,6 +9,6 @@ fi
 
 echo "starting locust at $host"
 
-locust --headless -H $host -f locustfile.py,irregular_load.py
-locust --headless -H $host -f locustfile.py,high_load.py
-locust --headless -H $host -f locustfile.py,steady_load.py
+locust $headless -H $host -f locustfile.py,irregular_load.py
+locust $headless -H $host -f locustfile.py,high_load.py
+locust $headless -H $host -f locustfile.py,steady_load.py
