0d973337d61754bebb0fff04f16e33987ecfb280
Ilja Rotar <kontakt@ilja-rotar.de>
2024-01-17 08:53:31 +0100

    irregular load shape deterministic

--- locust/irregular_load.py
+++ locust/irregular_load.py
@@ -1,9 +1,17 @@
 from locust import LoadTestShape
 import random
+import math
 
 class IrregularLoadShape(LoadTestShape):
     duration = 3600
-    max_users = 100
+    users = [
+        10, 90, 24, 75, 38, 99, 14,
+        46, 62, 23, 71, 12, 76, 53,
+        19, 94, 64, 72, 23, 86, 16,
+        85, 34, 26, 23, 95, 12, 43,
+        90, 54, 23, 81, 45, 23, 91,
+        74, 29, 56, 74, 100, 8, 34
+    ]
 
     def tick(self):
         run_time = self.get_run_time()
@@ -12,8 +20,5 @@ class IrregularLoadShape(LoadTestShape):
         if run_time >= self.duration:
             return None
 
-        if run_time % 100 < 1:
-            users = random.randint(0, self.max_users)
-            return (users, users)
-
-        return (current_users, 1)
+        index = math.floor(run_time/self.duration * len(self.users))
+        return (self.users[index], self.users[index])
